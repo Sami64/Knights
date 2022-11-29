468bc64235b2fef1d7e7750145359a402d6862e7
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-11-29 19:01:06 +0000

    intro anims and audio on point

--- Assets/Scripts/Narrator.cs
+++ Assets/Scripts/Narrator.cs
@@ -6,21 +6,38 @@ public class Narrator : MonoBehaviour
 {
     [SerializeField]
     AudioClip introSpeech;
+
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         StartCoroutine(PlayIntroSpeech());
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         
+            
+        
+
+        
     }
 
     IEnumerator PlayIntroSpeech()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(0.5f);
         GetComponent<AudioSource>().PlayOneShot(introSpeech);
+        animator.SetTrigger("Talk");
+        Invoke("SitDown", introSpeech.length);
+        
+    }
+
+    void SitDown()
+    {
+        animator.SetTrigger("Sit");
     }
 }
